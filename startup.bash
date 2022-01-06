7329211c2568b58cf37bd9b90051c7fbd35d848f
Martin <martin.leon@globant.com>
2022-01-05 18:53:49 -0600

    Feature: [WE-1.01] Update Startup Bash

--- startup.bash
+++ startup.bash
@@ -1,8 +1,11 @@
 #! /usr/bin/env bash
-
-source api/scrapping-env/bin/activate
+source scrapping-env/bin/activate
 #source ~/.bash_profile
 
 #echo $URL_API 
 #echo $ACCESS_KEY
+
+cd api/app
+python init.py
+
 #gunicorn --bind 127.0.0.1:5000 wsgi --threads 12 --worker-connections 100
