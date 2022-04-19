2b5f8f541dd9ac5917ab175e0243b62416ea36e4
malvabombom <alquimista3301@gmail.com>
2022-04-19 01:26:02 -0500

    Add Scrappi Styles

--- web-app/src/App.tsx
+++ web-app/src/App.tsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Scrappi</h1>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          We find hight-quality data for your projects.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
